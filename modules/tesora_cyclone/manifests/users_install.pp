85716ca508d478f584670070a70577cf0f14092f
Brian Hunter <bhunter@tesora.com>
2016-12-15 19:30:04 -0500

    first pass at nodepool
    site.pp search and replace openstack.org hostnames
    site.pp search and replace project config repo.
    nodepool clouds config
    add tesora users
    
    Change-Id: I7344c66b5122def5e9d0d369eecb193d937dc6df

--- modules/tesora_cyclone/manifests/users_install.pp
+++ modules/tesora_cyclone/manifests/users_install.pp
@@ -30,32 +30,18 @@ class tesora_cyclone::users_install (
     ## NOTE: This list is arranged in order of chronological precedence,
     ##       additions should be appended to the end.
     realize (
-      User::Virtual::Localuser['mordred'],
-      User::Virtual::Localuser['corvus'],
-      User::Virtual::Localuser['clarkb'],
-      User::Virtual::Localuser['fungi'],
-      User::Virtual::Localuser['slukjanov'],
-      User::Virtual::Localuser['elizabeth'],
-      User::Virtual::Localuser['jhesketh'],
-      User::Virtual::Localuser['nibz'],
-      User::Virtual::Localuser['yolanda'],
-      User::Virtual::Localuser['pabelanger'],
-      User::Virtual::Localuser['rcarrillocruz'],
-      User::Virtual::Localuser['ianw'],
+      User::Virtual::Localuser['andrew'],
+      User::Virtual::Localuser['amrith'],
+      User::Virtual::Localuser['bhunter'],
+      User::Virtual::Localuser['doug'],
+      User::Virtual::Localuser['iduodu'],
     )
   } else {
-      user::virtual::disable{'mordred':}
-      user::virtual::disable{'corvus':}
-      user::virtual::disable{'clarkb':}
-      user::virtual::disable{'fungi':}
-      user::virtual::disable{'slukjanov':}
-      user::virtual::disable{'elizabeth':}
-      user::virtual::disable{'jhesketh':}
-      user::virtual::disable{'nibz':}
-      user::virtual::disable{'yolanda':}
-      user::virtual::disable{'pabelanger':}
-      user::virtual::disable{'rcarrillocruz':}
-      user::virtual::disable{'ianw':}
+      user::virtual::disable['andrew']
+      user::virtual::disable['amrith']
+      user::virtual::disable['bhunter']
+      user::virtual::disable['doug']
+      user::virtual::disable['iduodu']
   }
 }
 
